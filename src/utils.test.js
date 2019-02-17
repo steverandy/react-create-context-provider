5cf5157658894f080f2aebcc0f108fe977b9a8e9
Steve Randy Tantra <steve.randy@gmail.com>
2019-02-17 09:05:49 +0800

    Add test

---
+++ src/utils.test.js
@@ -0,0 +1,10 @@
+let {pascalCase} = require("./utils");
+
+describe("utils", () => {
+  test("pascalCase", () => {
+    expect(pascalCase("variable_name")).toBe("VariableName");
+    expect(pascalCase("variable-name")).toBe("VariableName");
+    expect(pascalCase("variableName")).toBe("VariableName");
+    expect(pascalCase("VariableName")).toBe("VariableName");
+  });
+});
